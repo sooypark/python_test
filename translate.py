bbb6078c7df8dc2755e26c2bf27cad8a677c61d2
sooypark <36060457+sooypark@users.noreply.github.com>
2018-02-02 17:14:51 +0900

    Update translate.py

--- translate.py
+++ translate.py
@@ -15,7 +15,7 @@ fileObj = codecs.open( "c:\\temp\\bigram-word-korean.txt", "r", "utf-8" )
 file_w_Obj = codecs.open("c:\\temp\\bigram-trans-word.txt", "a", "utf-8")
 
 u = fileObj.readlines()
-
+#if the total size over 15K then error occurs, therefore for 400 items would be run
 for i in u[:400]:
     translated_word = translator.translate(i)
     data = "%s -> %s\n" %(translated_word.origin, translated_word.text)
